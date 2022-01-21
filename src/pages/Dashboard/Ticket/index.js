51905fa63af3496d04c1e69e1da176ffcdb49e8a
camilocoelhogomes <camilo.coelho.gomes@gmail.com>
2022-01-21 07:43:44 -0300

    hotel type appearing according with the ticket type

--- src/pages/Dashboard/Ticket/index.js
+++ src/pages/Dashboard/Ticket/index.js
@@ -1,14 +1,23 @@
+import { useTicket } from "../../../contexts/TicketContext";
 import SessionLetter from "../../../layouts/SessionLetter";
 import TicketCards from "./TicketCards";
 export default function Ticket() {
+  //dados mocados que devem vir do servidor
+  const { ticketInfo } = useTicket();
   const ticketTypes = [
     { id: 0, name: "Presencial", price: 250 },
     { id: 1, name: "Online", price: 100 }
   ];
-  const hotelTypes = [
-    { id: 0, name: "Sem Hotel", price: 0 },
-    { id: 1, name: "Com Hotel", price: 350 }
-  ];
+  const hotelTypes = () => {
+    if (ticketInfo.userTicketType.id === 0) {
+      return [
+        { id: 0, name: "Sem Hotel", price: 0 },
+        { id: 1, name: "Com Hotel", price: 350 }
+      ];
+    };
+    return [];
+  };
+  //fim dos dados mocados que devem vir do servidor
   return (
     <>
       <SessionLetter>Ingresso e Pagamento</SessionLetter>
@@ -17,11 +26,14 @@ export default function Ticket() {
         ticketReference={"userTicketType"}
         ticketTypes={ticketTypes}
       />
-      <TicketCards
-        title='Ótimo, agora escolhe sua modalidade de hospedagem'
-        ticketTypes={hotelTypes}
-        ticketReference={"userHotel"}
-      />
+      {
+        !hotelTypes().length ? <></> :
+          <TicketCards
+            title='Ótimo, agora escolhe sua modalidade de hospedagem'
+            ticketTypes={hotelTypes()}
+            ticketReference={"userHotel"}
+          /> 
+      }
     </>
   );
 }
