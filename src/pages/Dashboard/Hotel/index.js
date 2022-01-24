3b778c37329aed2b94faa75d3d41e33d9c119afb
Andrezza Souza <andrezzasouza@id.uff.br>
2022-01-24 02:20:28 -0300

    Preparar layout

--- src/pages/Dashboard/Hotel/index.js
+++ src/pages/Dashboard/Hotel/index.js
@@ -1,3 +1,32 @@
+import { useEffect, useState } from "react";
+import SessionLetter from "../../../layouts/SessionLetter";
+import ShowNoHotel from "./ShowNoHotel";
+import ShowSummary from "./ShowSummary";
+import useApi from "../../../hooks/useApi";
+
 export default function Hotel() {
-  return "Hotel: Em breve!";
+  const [hotelOptions, setHotelOptions] = useState({});
+  const { hotel } = useApi();
+
+  useEffect(() => {
+    setHotelOptions({
+      ...hotelOptions,
+      ticketType: "Presencial",
+      thereIsHotel: true,
+      paymentDone: true,
+      guests: 2
+    });
+
+    hotel
+      .getHotelInformation()
+      .then(() => {})
+      .catch(() => {});
+  }, []);
+
+  return (
+    <>
+      <SessionLetter>Escolha de hotel e quarto</SessionLetter>
+      {hotelOptions.paymentDone && hotelOptions.thereIsHotel ? (<ShowSummary hotelOptions={hotelOptions}/>) : (<ShowNoHotel hotelOptions={hotelOptions}/>)}
+    </>
+  );
 }
