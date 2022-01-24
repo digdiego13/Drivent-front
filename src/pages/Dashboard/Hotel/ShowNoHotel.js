7d9c7ab7cd3f909436a2d2b4170804563a853076
Andrezza Souza <andrezzasouza@id.uff.br>
2022-01-23 23:01:45 -0300

    Adicionar telas de hotel nao disponivel

---
+++ src/pages/Dashboard/Hotel/ShowNoHotel.js
@@ -0,0 +1,30 @@
+import styled from "styled-components";
+
+export default function ShowNoHotel({ hotelOptions } ) {
+  return (
+    <MessageContainer>
+      {hotelOptions.ticketType === "Online" || !hotelOptions.thereIsHotel ? (
+        <AlertText width="474px">Sua modalidade de ingresso não inclui hospedagem <br/> Prossiga para a escolha de atividades</AlertText>
+      ) : (
+        <AlertText width="414px">Você precisa ter confirmado pagamento antes de fazer a escolha de hospedagem</AlertText>
+      )}
+    </MessageContainer>
+  );
+}
+
+const MessageContainer = styled.section`
+  width: 100%;
+  height: calc(100% - 91px);
+  display: flex;
+  align-items: center;
+  justify-content: center;
+`;
+
+const AlertText = styled.p`
+  font-family: "Roboto", sans-serif;
+  width: ${(props) => props.width};
+  font-size: 20px;
+  line-height: 23px;
+  text-align: center;
+  color: #8E8E8E;
+`;
--- src/pages/Dashboard/Hotel/index.js
+++ src/pages/Dashboard/Hotel/index.js
@@ -1,3 +1,22 @@
+import { useEffect, useState } from "react";
+import SessionLetter from "../../../layouts/SessionLetter";
+import ShowNoHotel from "./ShowNoHotel";
+
 export default function Hotel() {
-  return "Hotel: Em breve!";
+  const [hotelOptions, setHotelOptions] = useState({});
+
+  useEffect(() => {
+    setHotelOptions({
+      ...hotelOptions,
+      ticketType: "Presencial",
+      thereIsHotel: false
+    });
+  }, []);
+
+  return (
+    <>
+      <SessionLetter>Escolha de hotel e quarto</SessionLetter>
+      <ShowNoHotel hotelOptions={hotelOptions}/>
+    </>
+  );
 }
